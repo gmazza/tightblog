fb20dfc977c354baf73c2e0282b5d6dceec9fd7e
Glen Mazza <glen.mazza@gmail.com>
2018-12-23 18:31:23 -0500

    Issue 973_1: Finished unit test coverage in rendering.cache.*

--- src/test/java/org/tightblog/rendering/cache/LazyExpiringCacheTest.java
+++ src/test/java/org/tightblog/rendering/cache/LazyExpiringCacheTest.java
@@ -26,44 +26,83 @@ import static org.junit.Assert.*;
 public class LazyExpiringCacheTest {
 
     private LazyExpiringCache cache;
+    private Instant twentySecondsAgo = Instant.now().minusSeconds(20);
+    private Instant twentySecondsLater = twentySecondsAgo.plusSeconds(40);
 
     @Before
     public void initialize() {
         cache = new LazyExpiringCache();
         cache.setMaxEntries(10);
         cache.setTimeoutSec(3600);
+        cache.setCacheHandlerId("testCache");
         cache.init();
+    }
+
+    @Test
+    public void testAccessors() {
+        assertEquals("testCache", cache.getCacheHandlerId());
+        assertEquals(10, cache.getMaxEntries());
     }
 
     @Test
     public void testReturnNonExpiredValue() {
         CachedContent testContent = new CachedContent(Template.ComponentType.ATOMFEED);
-        Instant twentySecondsAgo = Instant.now().minusSeconds(20);
         cache.put("abc", testContent);
         assertEquals(testContent, cache.get("abc", twentySecondsAgo));
+        assertNotEquals(0, cache.getEstimatedSize());
+
+        // test invalidate empties cache
+        cache.invalidateAll();
+        assertNull(cache.get("abc", twentySecondsAgo));
+
+        // test cache stats are non-zero
+        assertEquals(2, cache.getCacheRequestCount());
+        assertEquals(1, cache.getCacheHitCount());
+        assertEquals(1, cache.getCacheMissCount());
+        assertEquals(0.5, cache.getCacheHitRate(), .001);
     }
 
     @Test
-    public void testReturnNullIfCacheDisabled() {
+    public void testDisabledCacheResults() {
         cache.setMaxEntries(0);
         cache.init();
         CachedContent testContent = new CachedContent(Template.ComponentType.ATOMFEED);
-        Instant twentySecondsAgo = Instant.now().minusSeconds(20);
         cache.put("abc", testContent);
         assertNull(cache.get("abc", twentySecondsAgo));
+        assertEquals(0, cache.getCacheRequestCount());
+        assertEquals(0, cache.getCacheHitCount());
+        assertEquals(0, cache.getCacheMissCount());
+        assertEquals(0, cache.getCacheHitRate(), .001);
+        assertEquals(0, cache.getEstimatedSize());
     }
 
     @Test
     public void testReturnNullForExpiredValue() {
         CachedContent testContent = new CachedContent(Template.ComponentType.ATOMFEED);
         cache.put("abc", testContent);
-        Instant twentySecondsLater = Instant.now().plusSeconds(20);
         assertNull(cache.get("abc", twentySecondsLater));
     }
 
     @Test
     public void testReturnNullForNoncachedValue() {
-        Instant twentySecondsAgo = Instant.now().minusSeconds(20);
         assertNull(cache.get("abc", twentySecondsAgo));
     }
+
+    @Test
+    public void testIncomingRequests() {
+        assertEquals(0, cache.getIncomingRequests());
+        cache.incrementIncomingRequests();
+        cache.incrementIncomingRequests();
+        cache.incrementIncomingRequests();
+        assertEquals(3, cache.getIncomingRequests());
+    }
+
+    @Test
+    public void testRequestsHandledBy304() {
+        assertEquals(0, cache.getRequestsHandledBy304());
+        cache.incrementRequestsHandledBy304();
+        cache.incrementRequestsHandledBy304();
+        assertEquals(2, cache.getRequestsHandledBy304());
+    }
+
 }
