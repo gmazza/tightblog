fd6cc1d1dbe2fdb61705a5245689ee4ad540170a
Glen Mazza <glen.mazza@gmail.com>
2019-12-09 00:35:58 -0500

    Issue 970: New endpoint for displaying code in GitHub

--- src/main/java/org/tightblog/rendering/controller/ExternalSourceController.java
+++ src/main/java/org/tightblog/rendering/controller/ExternalSourceController.java
@@ -6,6 +6,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.CacheControl;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +20,7 @@ import org.springframework.web.client.RestTemplate;
 import org.tightblog.domain.SharedTemplate;
 import org.tightblog.domain.Template;
 import org.tightblog.rendering.cache.CachedContent;
+import org.tightblog.rendering.cache.LazyExpiringCache;
 import org.tightblog.rendering.thymeleaf.ThymeleafRenderer;
 
 import javax.servlet.http.HttpServletRequest;
@@ -27,6 +30,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.regex.Pattern;
 
@@ -39,17 +43,19 @@ public class ExternalSourceController {
 
     private static Logger log = LoggerFactory.getLogger(ExternalSourceController.class);
 
-    private ThymeleafRenderer thymeleafRenderer;
-
+    private LazyExpiringCache githubSourceCache;
     private Set<Pattern> whitelistPatterns = new HashSet<>();
+    private ThymeleafRenderer thymeleafRenderer;
 
     public static final String PATH = "/tb-ui/rendering/external";
 
     private static final MediaType APP_JAVASCRIPT = new MediaType("application", "javascript");
 
     public ExternalSourceController(@Qualifier("standardRenderer") ThymeleafRenderer thymeleafRenderer,
-                                    @Value("#{'${external.github.whitelist:}'.split(',')}")
-                                            String githubSourceWhitelist) {
+                                    @Value("#{'${external.github.whitelist:}'.split(',')}") String githubSourceWhitelist,
+                                    LazyExpiringCache githubSourceCache) {
+
+        this.githubSourceCache = githubSourceCache;
         this.thymeleafRenderer = thymeleafRenderer;
 
         if (!StringUtils.isBlank(githubSourceWhitelist)) {
@@ -64,11 +70,11 @@ public class ExternalSourceController {
     }
 
     @GetMapping(path = "/github/**")
-    ResponseEntity<String> getGithubSource(HttpServletRequest request,
-                                           @RequestParam(value = "start", required = false) Integer startLine,
-                                           @RequestParam(value = "end", required = false) Integer endLine,
-                                           @RequestParam(value = "height", required = false) Integer height,
-                                           @RequestParam(value = "linenums", required = false, defaultValue = "true")
+    ResponseEntity<Resource> getGithubSource(HttpServletRequest request,
+                                             @RequestParam(value = "start", required = false) Integer startLine,
+                                             @RequestParam(value = "end", required = false) Integer endLine,
+                                             @RequestParam(value = "height", required = false) Integer height,
+                                             @RequestParam(value = "linenums", required = false, defaultValue = "true")
                                                    boolean showLinenums
     ) throws IOException {
 
@@ -91,96 +97,120 @@ public class ExternalSourceController {
             return ResponseEntity.notFound().build();
         }
 
-        // path2 = mazza/blog-samples/raw/master/...
+        // path2 = gmazza/blog-samples/raw/master/...
         String path2 =  path.substring(projectPosition + 1);
 
-        // need to check if path2 passes whitelist if latter defined
-        if (whitelistPatterns.size() > 0) {
-            boolean allowed = false;
-            for (Pattern pattern : whitelistPatterns) {
-                if (pattern.matcher(path2).matches()) {
-                    allowed = true;
-                    break;
+        // Can check and return from cache before whitelist.  Any change in whitelist
+        // status for a request occurs only after app reboot, which means cache would be
+        // empty anyway for a newly blocked URL.
+        String cacheKey = generateKey(path2, startLine, endLine, height, showLinenums);
+        CachedContent response = githubSourceCache.get(cacheKey, null);
+
+        if (response == null) {
+
+            // need to check if path2 passes whitelist if latter defined
+            if (whitelistPatterns.size() > 0) {
+                boolean allowed = false;
+                for (Pattern pattern : whitelistPatterns) {
+                    if (pattern.matcher(path2).matches()) {
+                        allowed = true;
+                        break;
+                    }
+                }
+                if (!allowed) {
+                    log.info("Requested Github path {} blocked by configured whitelist", path2);
+                    return ResponseEntity.notFound().build();
                 }
             }
-            if (!allowed) {
-                log.info("Requested Github path {} blocked by configured whitelist", path2);
+
+            // change "/blob/" to "/raw/" if necessary
+            int rawPositionBefore = StringUtils.ordinalIndexOf(path2, "/", 2);
+            int rawPositionAfter = StringUtils.ordinalIndexOf(path2, "/", 3);
+            int branchPositionAfter = StringUtils.ordinalIndexOf(path2, "/", 4);
+            int lastSlash = path2.lastIndexOf("/");
+
+            if (branchPositionAfter == -1) {
                 return ResponseEntity.notFound().build();
             }
-        }
 
-        // change "/blob/" to "/raw/" if necessary
-        int rawPositionBefore = StringUtils.ordinalIndexOf(path2, "/", 2);
-        int rawPositionAfter = StringUtils.ordinalIndexOf(path2, "/", 3);
-        int branchPositionAfter = StringUtils.ordinalIndexOf(path2, "/", 4);
-        int lastSlash = path2.lastIndexOf("/");
+            String filePath = path2.substring(branchPositionAfter + 1);
+            String fileName = path2.substring(lastSlash + 1);
 
-        if (branchPositionAfter == -1) {
-            return ResponseEntity.notFound().build();
-        }
+            String githubRawURL = String.format("https://www.github.com/%s/raw/%s",
+                    path2.substring(0, rawPositionBefore), path2.substring(rawPositionAfter + 1));
 
-        String filePath = path2.substring(branchPositionAfter + 1);
-        String fileName = path2.substring(lastSlash + 1);
+            String githubBlobURL = String.format("https://www.github.com/%s/blob/%s",
+                    path2.substring(0, rawPositionBefore), path2.substring(rawPositionAfter + 1));
 
-        String githubRawURL = String.format("https://www.github.com/%s/raw/%s",
-            path2.substring(0, rawPositionBefore), path2.substring(rawPositionAfter + 1));
+            RestTemplate rt = new RestTemplate();
+            String sourceCode = rt.getForObject(githubRawURL, String.class, new HashMap<>());
 
-        String githubBlobURL = String.format("https://www.github.com/%s/blob/%s",
-                path2.substring(0, rawPositionBefore), path2.substring(rawPositionAfter + 1));
+            if (sourceCode == null) {
+                return ResponseEntity.notFound().build();
+            }
 
-        RestTemplate rt = new RestTemplate();
-        String sourceCode = rt.getForObject(githubRawURL, String.class, new HashMap<>());
+            Map<String, Object> model = new HashMap<>();
+            if (height != null) {
+                model.put("height", height);
+            }
 
-        if (sourceCode == null) {
-            return ResponseEntity.notFound().build();
-        }
-
-        Map<String, Object> model = new HashMap<>();
-        if (height != null) {
-            model.put("height", height);
             model.put("startLine", 1);
             model.put("showLinenums", showLinenums);
             model.put("rawUrl", githubRawURL);
             model.put("blobUrl", githubBlobURL);
             model.put("filePath", filePath);
             model.put("fileName", fileName);
-        }
 
-        // if requested show specific lines only (\n is a line indicator), first line is 1 or 0, second line is 2.
-        if (endLine != null && endLine >= 0 && (startLine == null || endLine >= startLine)) {
-            int end = StringUtils.ordinalIndexOf(sourceCode, "\n", endLine);
-            if (end > -1) {
-                sourceCode = sourceCode.substring(0, end);
+            // if requested show specific lines only (\n is a line indicator), first line is 1 or 0, second line is 2.
+            if (endLine != null && endLine >= 0 && (startLine == null || endLine >= startLine)) {
+                int end = StringUtils.ordinalIndexOf(sourceCode, "\n", endLine);
+                if (end > -1) {
+                    sourceCode = sourceCode.substring(0, end);
+                }
             }
-        }
 
-        if (startLine != null && startLine > 1 && (endLine == null || startLine <= endLine)) {
-            int beginning = StringUtils.ordinalIndexOf(sourceCode, "\n", startLine - 1);
-            if (beginning > -1) {
-                sourceCode = sourceCode.substring(beginning);
-                model.put("startLine", startLine);
+            if (startLine != null && startLine > 1 && (endLine == null || startLine <= endLine)) {
+                int beginning = StringUtils.ordinalIndexOf(sourceCode, "\n", startLine - 1);
+                if (beginning > -1) {
+                    sourceCode = sourceCode.substring(beginning);
+                    model.put("startLine", startLine);
+                }
             }
+
+            model.put("githubSource", sourceCode);
+
+            Template template = new SharedTemplate("github-source", Template.Role.JAVASCRIPT);
+            response = thymeleafRenderer.render(template, model);
+
+            String content = new String(response.getContent(), StandardCharsets.UTF_8);
+
+            // removing all "\n": ensure document.write() content on one line (required)
+            // replacing with "\\n" : place new lines in output html, suitable for line breaks under <pre> tags.
+            content = content.replace("\n", "\\n");
+
+            // important: document.write(...) must be on one line, no matter how long
+            content = String.format("document.write('%s');", content);
+
+            response.setContent(content.getBytes(StandardCharsets.UTF_8));
+
+            githubSourceCache.put(cacheKey, response);
         }
-
-        model.put("githubSource", sourceCode);
-
-        Template template = new SharedTemplate("github-source", Template.Role.JAVASCRIPT);
-        CachedContent rendererOutput = thymeleafRenderer.render(template, model);
-
-        String content = new String(rendererOutput.getContent(), StandardCharsets.UTF_8);
-
-        // removing all "\n": ensure document.write() content on one line (required)
-        // replacing with "\\n" : place new lines in output html, suitable for line breaks under <pre> tags.
-        content = content.replace("\n", "\\n");
-
-        // important: document.write(...) must be on one line, no matter how long
-        String response = String.format("document.write('%s');", content);
 
         return ResponseEntity.ok()
                 .contentType(APP_JAVASCRIPT)
-                .contentLength(response.length())
+                .contentLength(response.getContent().length)
                 .cacheControl(CacheControl.noCache())
-                .body(response);
+                .body(new ByteArrayResource(response.getContent()));
+    }
 
+    private String generateKey(String filePath, Integer startLine, Integer endLine, Integer height, boolean showLinenums) {
+        StringBuilder key = new StringBuilder();
+        key.append(filePath);
+
+        Optional.ofNullable(startLine).ifPresent(s -> key.append("/s=").append(s));
+        Optional.ofNullable(endLine).ifPresent(e -> key.append("/e=").append(e));
+        Optional.ofNullable(height).ifPresent(h -> key.append("/h=").append(h));
+        key.append("/l=").append(showLinenums);
+        return key.toString();
     }
 }
