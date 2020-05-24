5fc996df8bd2bc73889ede5e7952e46038fb4f0e
Glen Mazza <glen.mazza@gmail.com>
2020-05-24 17:14:21 -0400

    Remove unused imports.

--- src/main/java/org/tightblog/bloggerui/controller/AdminController.java
+++ src/main/java/org/tightblog/bloggerui/controller/AdminController.java
@@ -23,10 +23,8 @@ package org.tightblog.bloggerui.controller;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
-import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 
