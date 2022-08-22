59e4a042c95243730be43d3e2d416e16b2412676
Glen Mazza <glen.mazza@gmail.com>
2022-08-22 05:31:48 -0400

    Issue1092 16 (#1131)
    * Additional Vue fixes.

--- src/client/src/views/UserEdit.vue
+++ src/client/src/views/UserEdit.vue
@@ -198,10 +198,10 @@ export default {
     },
     updateUser: function () {
       this.messageClear();
-      var userData = {};
+      const userData = {};
       userData.user = this.userBeingEdited;
       userData.credentials = this.userCredentials;
-      var urlToUse = this.sessionInfo.authenticatedUser
+      const urlToUse = this.sessionInfo.authenticatedUser
         ? "/tb-ui/authoring/rest/userprofile/" +
           this.sessionInfo.authenticatedUser.id
         : "/tb-ui/register/rest/registeruser";
--- src/main/webapp/WEB-INF/jsps/tiles/footer.jsp
+++ src/main/webapp/WEB-INF/jsps/tiles/footer.jsp
@@ -17,5 +17,4 @@
 --%>
 <%@ include file="/WEB-INF/jsps/tightblog-taglibs.jsp" %>
 
-<!--script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script-->
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
